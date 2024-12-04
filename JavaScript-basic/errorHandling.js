d9059c952a7cb2d37e25f02bcc9907d85e01d70f
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-03 23:51:40 -0500

    error handling

---
+++ JavaScript-basic/errorHandling.js
@@ -0,0 +1,16 @@
+let a =5, b= 4;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+
+try{
+    console.log(a+c);
+}catch(err){
+    console.log(err);
+}
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
