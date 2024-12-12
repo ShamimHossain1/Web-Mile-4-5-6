7a5ffe88cc0d3aad725950601195c68a6e589949
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-12 00:29:51 -0500

    array spread operator

--- JavaScript-basic/destructuting.js
+++ JavaScript-basic/destructuting.js
@@ -6,13 +6,13 @@ const  student  ={
 }
 
 const {name, age, grade} = student;
-console.log(name, age, grade);
+// console.log(name, age, grade);
 
 
 const number = [10, 430];
 
 const [first, second] = number;
-console.log(first, second);
+// console.log(first, second);
 
 
 function doubleThem(a, b){
@@ -20,4 +20,15 @@ function doubleThem(a, b){
 }
 
 const [prothom, ditiyo] = doubleThem(6, 9);
-console.log(prothom, ditiyo);
+// console.log(prothom, ditiyo);
+
+
+const numbers = [0, 343, 3, 234, 67823, 4784];
+const num = numbers;
+const newNum = [...numbers];
+newNum[3] = 6969;
+num[1] = 420;
+numbers[0] = 69;
+console.log(num);
+console.log(numbers);
+console.log(newNum);
