ef60003061d866d2757d4b7dc5c2de4c4e8c9699
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-12 21:18:34 -0500

    class

---
+++ JavaScript-basic/class/class2.js
@@ -0,0 +1,30 @@
+class Product{
+
+    country = 'Bangladesh';
+   speak(talk){
+    console.log(`talking about ${talk}`);
+   }
+
+   constructor(name){
+    this.name = name;
+   }
+}
+
+const lenovo = new Product("lenovo yoga");
+lenovo.speak("Ai Chip")
+console.log(lenovo);
+
+
+class Teacher{
+    constructor(name,age, subject){
+        this.name = name;
+        this.age = age;
+        this.subject = subject;
+        }
+    lecture(){
+        console.log("Teacher is Teaching Math");
+    }
+}
+
+const Sauda = new Teacher("Sauda", "21", "Communication" );
+console.log(Sauda);
