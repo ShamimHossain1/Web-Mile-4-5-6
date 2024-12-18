9c077e41bf163336378905806e3e3a70b7d3179e
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-17 23:08:22 -0500

    `Added search functionality with loading spinner and show all button`

--- Phone_Hunter/script.js
+++ Phone_Hunter/script.js
@@ -1,18 +1,38 @@
-const loadPhone = async (searchText)=>{
+const loadPhone = async (searchText, isShow) => {
+    // Show loading spinner
+    loadingSpinner(true);
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     // console.log(data.data)
-    displayPhones(data.data);
+    displayPhones(data.data, isShow);
 }
 
-const displayPhones = phones =>{
-    const phoneContainer = document.getElementById('phone-container');
-    phoneContainer.innerHTML = '';
+
+
+const phoneContainer = document.getElementById('phone-container');
+
+const displayPhones = (phones, isShow) => {
+   
+    const showBtn = document.getElementById("show-btn");
+    if (phones.length > 12 && !isShow) {
+        showBtn.classList.remove("hidden");
+    } else {
+        showBtn.classList.add("hidden");
+    }
+
+if(!isShow){
+    phones = phones.slice(0, 12);
+   
+}
+
+
+    // console.log(phones.length);
+
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-base-100 w-96 shadow-xl`;
-        phoneCard.innerHTML=`
+        phoneCard.innerHTML = `
                         <figure>
                     <img src="${phone.image}" />
                 </figure>
@@ -21,20 +41,54 @@ const displayPhones = phones =>{
                     <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
                     <h2 id="price" class="text-center font-bold text-2xl ">$999</h2>
                     <div class="card-actions justify-center ">
-                        <button class="btn btn-primary">Show Details</button>
+                        <button onclick="showDetails()" class="btn btn-primary">Show Details</button>
                     </div>
                 </div>
         `;
         phoneContainer.appendChild(phoneCard);
     });
+    // Hide loading spinner
+    loadingSpinner(false);
 }
 
-const handleSearch = ()=>{
-    
+const handleSearch = (isShow) => {
+    phoneContainer.innerHTML = '';
     const searchField = document.getElementById('search-field');
     const searchValue = searchField.value;
-    console.log(searchValue);
-    loadPhone(searchValue);
+
+    if(searchValue){
+        loadPhone(searchValue, isShow);
+    }
+    else{
+        loadPhone("a", isShow);
+        }
+    // console.log(searchValue);
+  
 }
 
-// loadPhone();
+
+const loadingSpinner = (isLoading) => {
+    const spinner = document.getElementById("loading-spinner");
+    const showBtn = document.getElementById("show-btn");
+
+    if (isLoading) {
+        spinner.classList.remove("hidden");
+        showBtn.classList.add("hidden");
+    }
+    else {
+        spinner.classList.add("hidden");
+    }
+}
+
+const showAll = ()=>{
+    handleSearch(true);
+}
+
+const showDetails =()=>{
+    console.log();
+}
+
+
+
+//Default Data Load
+loadPhone("a");
