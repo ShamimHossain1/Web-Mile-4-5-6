5f6b81dc2fe487a751fb2e70e34ded03bd9ffcc8
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-17 23:34:24 -0500

    display phone details in a modal

--- Phone_Hunter/script.js
+++ Phone_Hunter/script.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShow) => {
+const loadPhone = async (searchText = 'a', isShow) => {
     // Show loading spinner
     loadingSpinner(true);
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
@@ -12,7 +12,7 @@ const loadPhone = async (searchText, isShow) => {
 const phoneContainer = document.getElementById('phone-container');
 
 const displayPhones = (phones, isShow) => {
-   
+
     const showBtn = document.getElementById("show-btn");
     if (phones.length > 12 && !isShow) {
         showBtn.classList.remove("hidden");
@@ -20,10 +20,10 @@ const displayPhones = (phones, isShow) => {
         showBtn.classList.add("hidden");
     }
 
-if(!isShow){
-    phones = phones.slice(0, 12);
-   
-}
+    if (!isShow) {
+        phones = phones.slice(0, 12);
+
+    }
 
 
     // console.log(phones.length);
@@ -41,7 +41,7 @@ if(!isShow){
                     <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
                     <h2 id="price" class="text-center font-bold text-2xl ">$999</h2>
                     <div class="card-actions justify-center ">
-                        <button onclick="showDetails()" class="btn btn-primary">Show Details</button>
+                        <button onclick="showDetails('${phone.slug}'); phone_Details.showModal()" class="btn btn-primary">Show Details</button>
                     </div>
                 </div>
         `;
@@ -51,7 +51,7 @@ if(!isShow){
     loadingSpinner(false);
 }
 
-const handleSearch = (isShow) => {
+const handleSearch = (isShow) => {     
     phoneContainer.innerHTML = '';
     const searchField = document.getElementById('search-field');
     const searchValue = searchField.value;
@@ -62,8 +62,7 @@ const handleSearch = (isShow) => {
     else{
         loadPhone("a", isShow);
         }
-    // console.log(searchValue);
-  
+
 }
 
 
@@ -80,15 +79,37 @@ const loadingSpinner = (isLoading) => {
     }
 }
 
-const showAll = ()=>{
+const showAll = () => {
     handleSearch(true);
 }
 
-const showDetails =()=>{
-    console.log();
+const showDetails = async (id) => {
+    const details_modal = document.getElementById('phone_Details');
+    details_modal.innerHTML = "";
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phoneDetails = data.data;
+    console.log(phoneDetails);
+
+    details_modal.innerHTML = `
+   
+            <div class="modal-box">
+           
+                    <img class="w-1/2 mx-auto p-6 " src="${phoneDetails.image}" />
+             
+                <h3 class="text-lg font-bold">${phoneDetails.name}</h3>
+                <p class="py-4">${phoneDetails.releaseDate}</p>
+                <div class="modal-action">
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn">Close</button>
+                    </form>
+                </div>
+            </div>
+      
+    
+    `;
 }
 
-
-
 //Default Data Load
-loadPhone("a");
+loadPhone();
